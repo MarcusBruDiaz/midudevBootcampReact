606a9186b37c5af2c5a3d5dde55e5cfd5c0c03c0
MarcusBruDiaz <marcos.bru@udea.edu.co>
2023-02-03 20:37:54 -0500

    resolucion de ejercicios 1.1 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,30 +1,85 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
+
+const Header = (props) =>{
+
+  console.log(props)
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h1 style={{color: props.color}}>
+        {props.course}
+      </h1>
     </div>
   )
 }
+
+const Part = (props)=>{
+  return(
+    <div>
+      <p style={{color:props.color}}>{props.name1} {props.amount1}</p>
+      <p>{props.name2} {props.amount2}</p>
+      <p>{props.name3} {props.amount3}</p>
+    </div>
+  )
+}
+
+const Content = (props) =>{
+  console.log(props)
+  return(
+    <div style={{color: props.color}}>
+      <Part  color="yellow" name1={props.parts[0].name} amount1={props.parts[0].exercises} />
+      <Part name2={props.parts[1].name} amount2={props.parts[1].exercises}/>
+      <Part name3={props.parts[2].name} amount3={props.parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Total = (props)=>{
+  console.log(props.parts[0].name)
+  return(
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
+    </div>
+  )
+}
+
+
+
+
+const App = () => {
+  // const-definitions
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <> 
+      <Header  color="red" course={course.name} />
+      <Content color="blue" parts={course.parts} /> 
+      <Total parts={course.parts} />
+    </>
+  )
+
+}
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
